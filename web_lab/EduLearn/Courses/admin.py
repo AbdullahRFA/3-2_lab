9327561c76b6c36a030b91edc4a40f9112a75b10
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-19 12:04:58 +0600

    added in model , classes courses, lesson, student and register them

--- web_lab/EduLearn/Courses/admin.py
+++ web_lab/EduLearn/Courses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Courses, Lesson, Student
 # Register your models here.
+admin.site.register(Courses)
+admin.site.register(Lesson)
+admin.site.register(Student)
--- web_lab/EduLearn/Courses/models.py
+++ web_lab/EduLearn/Courses/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    duration = models.IntegerField(help_text="Duration in hours")
+    thumbnail = models.ImageField(upload_to='course_thumbnail/',null=True,blank=True)
+    
+    def __str__(self):
+        return self.title + ' ( ' + self.duration+ ' ) '
+    
+    
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    course = models.ForeignKey(Courses,on_delete=models.CASCADE,related_name='lessons')
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    enrolled_courses = models.ManyToManyField(Courses, related_name='students')
+    
+    def __str__(self):
+        return self.name
