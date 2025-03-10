4a6d8f1938d5f4183fd965628c2906286f124572
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-10 07:43:26 +0600

    customize admin pannel for seeing data properly of course, lesson and student models

--- web_lab/EduLearn/Courses/admin.py
+++ web_lab/EduLearn/Courses/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import Courses, Lesson, Student
+
 # Register your models here.
-admin.site.register(Courses)
-admin.site.register(Lesson)
-admin.site.register(Student)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'duration', 'thumbnail']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'course']
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'get_enrolled_courses']
+
+    def get_enrolled_courses(self, obj):
+        return ", ".join([course.title for course in obj.enrolled_courses.all()])  # Display course titles
+    get_enrolled_courses.short_description = 'Enrolled Courses'  # Column header
+
+admin.site.register(Courses, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Student, StudentAdmin)
