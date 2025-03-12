f0a9e3250295457a8b682193c25f39dc525642bf
AbdullahRFA <shakibrybmn@gmail.com>
2025-03-12 14:06:16 +0600

    modify admin administration text

--- web_lab/EduLearn/EduLearn/urls.py
+++ web_lab/EduLearn/EduLearn/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
+admin.site.site_header = "Abdullah Admin"
+admin.site.site_title = "Edu_Learn"
+admin.site.index_title = "Welcome to Edu_Learn"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
